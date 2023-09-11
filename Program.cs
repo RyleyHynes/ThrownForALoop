f5c7c068a46f6d4a5aa674b53eef1e509426cfc5
NSS Installs Token <ryleyhynes@gmail.com>
2023-09-11 08:44:14 -0500

    thrown for a loop first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string response = Console.ReadLine().Trim();
+
+while (string.IsNullOrEmpty(response))
+{
+    Console.WriteLine("You didn't choose anything, try again!");
+    response = Console.ReadLine().Trim();
+}
+
+Console.WriteLine($"You chose: {response}");
